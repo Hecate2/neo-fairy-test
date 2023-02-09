b84b365e7c0bc51409dbe602fb06ac6322045d15
Hecate2 <2474101468@qq.com>
2023-02-09 12:11:25 +0800

    ipv6 support; multi fairy server support

--- FairyPlugin.cs
+++ FairyPlugin.cs
@@ -1,6 +1,7 @@
 using Neo.ConsoleService;
 using Neo.SmartContract.Native;
 using Microsoft.Extensions.Configuration;
+using System.Runtime.Serialization.Formatters;
 
 namespace Neo.Plugins
 {
@@ -21,7 +22,7 @@ namespace Neo.Plugins
 
         Settings settings;
         NeoSystem system;
-        Fairy fairy;
+        List<Fairy> fairyServers = new();
 
         protected override void Configure()
         {
@@ -31,58 +32,79 @@ namespace Neo.Plugins
 
         protected override void OnSystemLoaded(NeoSystem system)
         {
-            RpcServerSettings s = settings.Servers.FirstOrDefault(p => p.Network == system.Settings.Network);
             this.system = system;
-            fairy = new(system, s);
-            fairy.StartRpcServer();
-            fairy.StartWebsocketServer();
+            bool hasServer = false;
+            foreach (RpcServerSettings s in settings.Servers)
+            {
+                if (s.Network == system.Settings.Network)
+                {
+                    hasServer = true;
+                    Fairy fairy = new(system, s);
+                    fairy.StartRpcServer();
+                    fairy.StartWebsocketServer();
+                    fairyServers.Add(fairy);
+                }
+            }
+            if (hasServer == false)
+            {
+                ConsoleHelper.Warning("No valid server from config. Using default!");
+                RpcServerSettings s = RpcServerSettings.Default;
+                Fairy fairy = new(system, s);
+                fairy.StartRpcServer();
+                fairy.StartWebsocketServer();
+                fairyServers.Add(fairy);
+            }
         }
 
         [ConsoleCommand("fairy", Category = "Fairy Commands", Description = "List Fairy snapshots")]
         private void OnFairyCommand()
         {
-            ConsoleHelper.Info("Test snapshots:");
-            if (fairy.sessionStringToFairySession.Keys.Count > 0)
+            foreach (Fairy fairy in fairyServers)
             {
-                Console.WriteLine("session name:\t\t\t");
-                foreach (string k in fairy.sessionStringToFairySession.Keys)
+                Console.WriteLine($">>> Fairy@{fairy.settings.BindAddress}:{fairy.settings.Port}");
+                ConsoleHelper.Info("Test snapshots:");
+                if (fairy.sessionStringToFairySession.Keys.Count > 0)
                 {
-                    ConsoleHelper.Info($"{k}\t\t\t{fairy.sessionStringToFairySession[k].ToString()}");
-                }
-            }
-            else
-            {
-                ConsoleHelper.Warning($"No test snapshot created!");
-            }
-            Console.WriteLine("------");
-
-            ConsoleHelper.Info("DebugInfo registration:");
-            if (fairy.contractScriptHashToSourceLineFilenames.Keys.Count > 0)
-            {
-                Console.Error.WriteLine($"test snapshot\t\t\tcontract name\t\t\tscript hash");
-                foreach (UInt160 k in fairy.contractScriptHashToSourceLineFilenames.Keys)
-                {
-                    string? contractName = null;
-                    string? testSession = null;
-                    foreach (string s in fairy.sessionStringToFairySession.Keys)
+                    Console.WriteLine("session name:\t\t\t");
+                    foreach (string k in fairy.sessionStringToFairySession.Keys)
                     {
-                        contractName = NativeContract.ContractManagement.GetContract(fairy.sessionStringToFairySession[s].engine.Snapshot, k)?.Manifest.Name;
-                        if (contractName != null)
-                        {
-                            testSession = s;
-                            break;
-                        }
+                        ConsoleHelper.Info($"{k}\t\t\t{fairy.sessionStringToFairySession[k].ToString()}");
                     }
-                    if (contractName == null)
-                        contractName = "Unknown Contract";
-                    ConsoleHelper.Info($"{testSession}\t\t\t\t{contractName}\t\t\t{k}");
-                    ConsoleHelper.Info(String.Join(", ", fairy.contractScriptHashToSourceLineFilenames[k]));
-                    Console.WriteLine("---");
                 }
-            }
-            else
-            {
-                ConsoleHelper.Warning($"No DebugInfo registration!");
+                else
+                {
+                    ConsoleHelper.Warning($"No test snapshot created!");
+                }
+                Console.WriteLine("------");
+
+                ConsoleHelper.Info("DebugInfo registration:");
+                if (fairy.contractScriptHashToSourceLineFilenames.Keys.Count > 0)
+                {
+                    Console.Error.WriteLine($"test snapshot\t\t\tcontract name\t\t\tscript hash");
+                    foreach (UInt160 k in fairy.contractScriptHashToSourceLineFilenames.Keys)
+                    {
+                        string? contractName = null;
+                        string? testSession = null;
+                        foreach (string s in fairy.sessionStringToFairySession.Keys)
+                        {
+                            contractName = NativeContract.ContractManagement.GetContract(fairy.sessionStringToFairySession[s].engine.Snapshot, k)?.Manifest.Name;
+                            if (contractName != null)
+                            {
+                                testSession = s;
+                                break;
+                            }
+                        }
+                        if (contractName == null)
+                            contractName = "Unknown Contract";
+                        ConsoleHelper.Info($"{testSession}\t\t\t\t{contractName}\t\t\t{k}");
+                        ConsoleHelper.Info(String.Join(", ", fairy.contractScriptHashToSourceLineFilenames[k]));
+                        Console.WriteLine("---");
+                    }
+                }
+                else
+                {
+                    ConsoleHelper.Warning($"No DebugInfo registration!");
+                }
             }
         }
     }
